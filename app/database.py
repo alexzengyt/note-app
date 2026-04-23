52a19a406dbd85497bc094f25ad5937b428fac89
alexzengyt <alexandra.yt.zeng@gmail.com>
2026-04-23 12:46:01 -0500

    update database config to use env variable

--- app/database.py
+++ app/database.py
@@ -1,6 +1,9 @@
 from sqlmodel import create_engine, Session
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL="postgresql://postgres:mypassword@localhost:5432/note-app"
+# DATABASE_URL="postgresql://postgres:mypassword@localhost:5432/note-app"
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:mypassword@localhost:5432/note-app")
 
 engine = create_engine(
     DATABASE_URL,
